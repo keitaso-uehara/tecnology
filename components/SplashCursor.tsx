ae6deb639fe06a4ac8980ec798228c7604c7a1f1
keitaso-uehara <keitaso2design@gmail.com>
2025-12-05 12:41:25 +0900

    Fix SplashCursor type error and canvas null checks

--- components/SplashCursor.tsx
+++ components/SplashCursor.tsx
@@ -146,7 +146,7 @@ function SplashCursor({
       fragmentShaderSource: any;
       programs: any[];
       activeProgram: any;
-      uniforms: any[];
+      uniforms: any;
 
       constructor(vertexShader: any, fragmentShaderSource: any) {
         this.vertexShader = vertexShader;
@@ -843,8 +843,8 @@ function SplashCursor({
       pointer.id = id;
       pointer.down = true;
       pointer.moved = false;
-      pointer.texcoordX = posX / canvas.width;
-      pointer.texcoordY = 1.0 - posY / canvas.height;
+      pointer.texcoordX = posX / canvas!.width;
+      pointer.texcoordY = 1.0 - posY / canvas!.height;
       pointer.prevTexcoordX = pointer.texcoordX;
       pointer.prevTexcoordY = pointer.texcoordY;
       pointer.deltaX = 0;
